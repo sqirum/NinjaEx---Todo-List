84a49c1c15041903fda9fabe9930e18ac29eda3c
sqirum <pauloslh94@gmail.com>
2022-09-26 23:28:09 +0100

    finish exercise

--- app.js
+++ app.js
@@ -32,16 +32,16 @@ list.addEventListener('click', e => {
 
 const filterTodos = (term) => {
     Array.from(list.children)
-        .filter((todo) => todo.textContent.includes(term) )
+        .filter((todo) => !todo.textContent.toLowerCase().includes(term) )
         .forEach((todo) => todo.classList.add('filtered'));
 
     Array.from(list.children)
-        .filter((todo) => todo.textContent.includes(term) )
+        .filter((todo) => todo.textContent.toLowerCase().includes(term) )
         .forEach((todo) => todo.classList.remove('filtered'));
 }
 
 // key up event
 search.addEventListener('keyup', () => {
-    const term = search.value.trim();
+    const term = search.value.trim().toLowerCase();
     filterTodos(term);
 })
